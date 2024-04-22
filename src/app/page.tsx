05e5b693a12c36df174c6b5aa3bc523e5dd2181b
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-04-22 13:26:01 +0600

    add new sections in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 import Navbar from '@/components/Navbar';
 import HeroSection from './_comps/HeroSection';
 import SkillsSection from './_comps/SkillsSection';
+import ProjectsSection from './_comps/ProjectsSection';
+import YoutubeSection from './_comps/YoutubeSection';
+import ContactSection from './_comps/ContactSection';
 
 export default function Page() {
   return (
     <main className="min-h-screen">
-      <Navbar activePage="home" />
+      <Navbar />
 
       <HeroSection />
 
       <SkillsSection />
+
+      <ProjectsSection />
+
+      <YoutubeSection />
+
+      <ContactSection />
     </main>
   );
 }
